4cab113df3e7c373c6e95ca8ed3ce702369fb141
prashant000000004 <py418850@gmail.com>
2025-02-23 16:54:31 +0530

    Ecommerce app

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,20 @@ import 'package:amaflip/services/constant.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:appwrite/appwrite.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey = publishableKey;
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  Client client = Client();
+  client.setProject('67bb02d900273bf0a205');
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
